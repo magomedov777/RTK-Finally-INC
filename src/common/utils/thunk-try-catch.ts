e5d07dadfe129ce90c8ef4cc49f1e303cbdb6391
Arkadiy <alibullterrier@gmail.com>
2024-05-18 00:00:34 +0300

    create: JSdoc for thunk-try-catch utills function

--- src/common/utils/thunk-try-catch.ts
+++ src/common/utils/thunk-try-catch.ts
@@ -4,6 +4,15 @@ import { BaseThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk'
 import { appActions } from 'app/app.reducer'
 import { BaseResponseType } from 'common/types'
 
+/**
+
+Executes a thunk function with try-catch error handling and dispatches appropriate actions.
+@template T - The type of the expected result from the logic function.
+@param {BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | BaseResponseType>} thunkAPI - The object containing the dispatch and rejectWithValue functions.
+@param {() => Promise<T>} logic - The logic function to be executed.
+@returns {Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>>} A promise that resolves to the result of the logic function or rejects with a value provided by thunkAPI.rejectWithValue.
+*/
+
 export const thunkTryCatch = async <T>(
   thunkAPI: BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | BaseResponseType>,
   logic: () => Promise<T>,
